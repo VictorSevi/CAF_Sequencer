eb46f9042ca1b7dde72e80af033d35b1bb8861a1
VICTOR SEVILLANO GAMARRA <vsevillano@caf.net>
2024-02-07 11:28:25 +0100

    WIP: vars detect

--- excel_parser/import_excel.py
+++ excel_parser/import_excel.py
@@ -14,7 +14,20 @@ file_path = askopenfilename(
 
 def devolver_text(text):
     return str(text)
-    
+
+def  suite_definition(line):
+    return True
+def case_definition(line):
+    return False
+
+def step_definition(line):
+    return False
+
+def autocheck_action(line):
+    return True
+
+def mancheck_action(line):
+    return True
  
 if file_path:
     # Load the selected Excel file
@@ -44,7 +57,6 @@ if file_path:
     #6 Empty Res
     #7 Empty Act
 
-    flag_case_descript=0
     flag_case_initials=0
     is_step=0
     is_case=0
@@ -52,56 +64,66 @@ if file_path:
     test_steps_list=[]
     test_action_list=[]
     test_cases_list=[]
+    test_suites_list=[]
 
     for line in arr:
-        if not("." in line.item(1)) and (not line.item(1)=="nan"):
+        #Si hay una definicon de suite se detecta y añade la estructura aquí
+
+        if suite_definition(line):#not("." in line.item(1)) and (not line.item(1)=="nan"):
             is_suite=1
-            test_case={
+            test_cases_list=[]
+            test_suite={
                 "name":line.item(2),
                 "id": line.item(1),
-                "description":"",
-                "initial_conditions":[],
-                "test_steps":[]
+                "Test_Cases":test_cases_list                
             }
-            flag_case_descript=1
+            test_suites_list.append(test_suite)
 
-        elif flag_case_descript==1:
-            test_case.update({"description":line.item(2)})
-            flag_case_descript=0
+
+        #Si hay una definicon de case se detecta y añade la estructura aquí
+            
+        elif case_definition(line):
+            is_case=1
             flag_case_initials=1
+            test_steps_list=[]
+            test_case={
+                "name":test_suite["name"],
+                "id": line.item(1),
+                "description":line.item(2),
+                "initial_conditions":[],
+                "test_steps":test_steps_list
+            }
+            test_cases_list.append(test_case)
+            
+        #Si en el paso anterior se definió un case ahora van las Initial conditions
 
         elif flag_case_initials==1:
             test_case.update({"initial_conditions":line.item(2).splitlines()[1:]})
             flag_case_initials=0
         
-        elif not (line.item(7)):
-                is_step=1
-                test_action_list=[]
-                test_step={"id":line.item(1),
-                           "test_actions":[]}
-                test_action_list.append({"text": line.item(2),"type":"MFA"})
-                test_step["test_actions"]=test_action_list
+        #Después vienen ya los steps (step, action, action.....)
+
+        elif step_definition(line):#not (line.item(7)):
+            is_step=1
+            test_action_list=[]
+            test_step={
+                "id":line.item(1),
+                "test_actions":test_action_list
+                }
+            test_action_list.append({"text": line.item(2),"type":"MFA"})
+            test_step["test_actions"]=test_action_list
             
 
-        if not (line.item(5)):
+        if autocheck_action(line):#not (line.item(5)):
             test_action_list.append({"text": line.item(3),"type":"ACA","variables":line.item(4).splitlines()})
             test_step["test_actions"]=test_action_list
             
-        elif not (line.item(6)):   
+        elif mancheck_action(line): #not (line.item(6)):   
             test_action_list.append({"text": line.item(3),"type":"MCA"})
             test_step["test_actions"]=test_action_list
         
-        
-        if is_step==1:
-            is_step=0
-            test_steps_list.append(test_step)
-            test_case["test_steps"]=test_steps_list
-
-        
-        #if is_case==1:
-        #    is_suite=0
-        #    test_cases_list.append(test_case)
-        #    test_suite["Test_Cases"]= test_cases_list
+        test_steps_list.append(test_step)
+            
     
     test_cases_list.append(test_case)
     test_suite["Test_Cases"]= test_cases_list
@@ -110,37 +132,6 @@ if file_path:
         json.dump(test_suite, outfile, indent = 4)
 
 
-    #json_str = json.dumps(test_step, indent=4)
-    #print(json_str)  
-
-
-        #elif not line.item(2) and line.item(2)
-
-
-           
-
-  
-    
-    #arr=df.to_numpy()
-    #x=arr.item(2,2)
-    #print(x)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #df.to_excel('modified_excel_file.xlsx', index=False)
- 
-    #print("File processed successfully!")
 else:
     print("No file selected.")
 
